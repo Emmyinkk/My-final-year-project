7c6e141292ecd4b4384371478d0e41dc34c875af
Emmanuel Fagbemi <olaoluwafagbemi@gmail.com>
2023-05-14 10:32:07 +0100

    addded path

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/https://github.com/Emmyinkk/My-final-year-project.git/" : "/",
+};
